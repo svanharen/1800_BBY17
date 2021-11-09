76a285a6fc8f275af5fe377f7dbefe2a77082f72
Sam <sambredenhof@gmail.com>
2021-11-08 16:28:35 -0800

    fixed errors, save some data to firebase

--- scripts/firebaseAPI.js
+++ scripts/firebaseAPI.js
@@ -1,10 +1,18 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBLYvL8ai6JyuzKldHLjRdZ0A_ahcYpaBM",
-  authDomain: "webapp-9f187.firebaseapp.com",
-  projectId: "webapp-9f187",
-  storageBucket: "webapp-9f187.appspot.com",
-  messagingSenderId: "271835665384",
-  appId: "1:271835665384:web:427ffcca0a4e89528aca5d"
+  // apiKey: "AIzaSyBLYvL8ai6JyuzKldHLjRdZ0A_ahcYpaBM",
+  // authDomain: "webapp-9f187.firebaseapp.com",
+  // projectId: "webapp-9f187",
+  // storageBucket: "webapp-9f187.appspot.com",
+  // messagingSenderId: "271835665384",
+  // appId: "1:271835665384:web:427ffcca0a4e89528aca5d"
+
+  apiKey: "AIzaSyC7nuhfSNakCZbjgmrvvNiSCLajfzNOV6w",
+  authDomain: "game-test-71f93.firebaseapp.com",
+  projectId: "game-test-71f93",
+  storageBucket: "game-test-71f93.appspot.com",
+  messagingSenderId: "947990707814",
+  appId: "1:947990707814:web:e26a68374c9e3b840877a7",
+  measurementId: "G-FDFN38B02P"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
