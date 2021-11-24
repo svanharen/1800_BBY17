76dfc462567b2c8edd81eb0f4850eb943e69a5c9
Sam <sambredenhof@gmail.com>
2021-11-24 13:32:28 -0800

    test

--- scripts/noti.js
+++ scripts/noti.js
@@ -1,32 +1,24 @@
-(async () => {
-    const icon = "./images/green.png";
+const icon = "./images/green.png";
 
-    if (Notification.permission === "granted")
-    {
-        showNotification("My Notification", "Hello World", icon);
-    }
-    else if (Notification.permission !== "denied")
-    {
+function notify() {
+    if (Notification.permission === "granted") {
+        showNotification("Don't Forget!", "Track your spending!", icon);
+    } else if (Notification.permission !== "denied") {
         Notification.requestPermission()
-                .then(permission => {
+            .then(permission => {
 
-                    if (permission === "granted")
-                    {
-                        showNotification("My Notification", "Hello World", icon);
-                    }
+                if (permission === "granted") {
+                    showNotification("My Notification", "Hello World", icon);
+                }
 
-                });
+            });
     }
+}
 
-    function showNotification(title, text) {
-        const notification = new Notification(title, {
-            body: text,
-            icon: icon
-        });
+function showNotification(title, text) {
+    const notification = new Notification(title, {
+        body: text,
+        icon: icon
+    });
 
-        notification.onclick = (event) => {
-            window.location.href = "https://appiomatic.com/blog";
-        };
-    }
-
-})();
+}
